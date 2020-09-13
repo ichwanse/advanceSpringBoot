49952594a09072d573201b03c1f79bd6ac3efaef
Ichwan Indocyber <ichwan100@gmail.com>
2020-09-13 18:51:44 +0700

    17%

--- src/main/java/com/springboot/learning/dao/FakeDataDao.java
+++ src/main/java/com/springboot/learning/dao/FakeDataDao.java
@@ -15,4 +15,31 @@ public class FakeDataDao implements UserDao{
         database.put(ikhwanUserUid, new User(ikhwanUserUid, "Ikhwan", "Suciadi", User.Gender.MALE, 28, "ichwan100@gmail.com"));
     }
 
+    @Override
+    public List<User> selectAllUsers() {
+        return new ArrayList<>(database.values());
+    }
+
+    @Override
+    public User selectUserByUserUid(UUID userUid) {
+        return database.get(userUid);
+    }
+
+    @Override
+    public int updateUser(User user) {
+        database.put(user.getUserUid(), user);
+        return 1;
+    }
+
+    @Override
+    public int deleteUserByUid(UUID userUid) {
+        database.remove(userUid);
+        return 1;
+    }
+
+    @Override
+    public int insertUser(UUID userUid, User user) {
+        database.put(userUid, user);
+        return 1;
+    }
 }
--- src/main/java/com/springboot/learning/service/UserService.java
+++ src/main/java/com/springboot/learning/service/UserService.java
@@ -1,17 +1,24 @@
 package com.springboot.learning.service;
 
+import com.springboot.learning.dao.UserDao;
 import com.springboot.learning.model.User;
 
 import java.util.List;
 import java.util.UUID;
 
 public class UserService {
+    private UserDao userDao;
 
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
     public List<User> getAllUsers() {
+        return null;
     }
 
     public User getUser(UUID userUid) {
+        return null;
     }
 
     public int updateUser(User user) {
